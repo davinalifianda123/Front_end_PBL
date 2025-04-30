b3396652d2cab7970f76c2992143380ca923c54a
danielaiyon <yonyondap23@gmail.com>
2025-04-30 08:19:00 +0700

    menambahkan barangseeder (soalnya engga bisa dipull dari main)

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Support\Facades\Storage;
+use App\Imports\BarangImport;
 
 class BarangSeeder extends Seeder
 {
@@ -13,6 +14,12 @@ class BarangSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $this->command->info('Importing roles from Excel...');
+
+        $disk = 'local';
+        $fileName = 'Barang.xlsx';
+        $filePath = Storage::disk($disk)->path($fileName);
+
+        Excel::import(new BarangImport, $filePath);
     }
 }
