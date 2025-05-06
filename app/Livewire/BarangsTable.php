1b7b4855c5aafdb662456f17191821ab5e030e55
Davin <dav1nalifianda@gmail.com>
2025-05-06 22:07:30 +0700

    install livewire

---
+++ app/Livewire/BarangsTable.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Livewire;
+
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use App\Models\Barang;
+
+class BarangsTable extends DataTableComponent
+{
+    protected $model = Barang::class;
+
+    public function configure(): void
+    {
+        $this->setPrimaryKey('id');
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make("Id", "id")
+                ->sortable(),
+            Column::make("Nama barang", "nama_barang")
+                ->sortable(),
+            Column::make("Id kategori barang", "id_kategori_barang")
+                ->sortable(),
+            Column::make("Flag", "flag")
+                ->sortable(),
+            Column::make("Created at", "created_at")
+                ->sortable(),
+            Column::make("Updated at", "updated_at")
+                ->sortable(),
+        ];
+    }
+}
