55308dbc6f55e2793136979d61aee58c19a3dda1
torz <imeliodas989@gmail.com>
2025-05-06 22:34:34 +0700

    Kategori Barang

---
+++ app/Livewire/KategoriBarangTable.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Livewire;
+
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use App\Models\KategoriBarang;
+
+class KategoriBarangTable extends DataTableComponent
+{
+    protected $model = KategoriBarang::class;
+
+    public function configure(): void
+    {
+        $this->setPrimaryKey('id');
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make("Id", "id")
+                ->sortable(),
+            Column::make("Nama kategori barang", "nama_kategori_barang")
+                ->sortable(),
+            Column::make("Flag", "flag")
+                ->sortable(),
+            Column::make("Created at", "created_at")
+                ->sortable(),
+            Column::make("Updated at", "updated_at")
+                ->sortable(),
+        ];
+    }
+}
