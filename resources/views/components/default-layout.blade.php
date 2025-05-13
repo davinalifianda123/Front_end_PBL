bf61317d1210fb282fed73d8f6f74f7086c0bf80
Davin <dav1nalifianda@gmail.com>
2025-05-13 11:14:33 +0700

    intall flowbite npm

--- resources/views/components/default-layout.blade.php
+++ resources/views/components/default-layout.blade.php
@@ -36,6 +36,7 @@
             </main>
         </div>
 
+        <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
         @livewireScripts
     </body>
 </html>
