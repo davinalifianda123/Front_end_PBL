9a246f346fa639424f2310bf651b1ef19ba8c56e
torz <imeliodas989@gmail.com>
2025-05-20 18:06:16 +0700

    Progres Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,62 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\KategoriBarang;
+use App\Models\Barang;
+use App\Models\User;
+use App\Models\GudangDanToko;
 
 class DashboardController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
-    }
+        $jumlahKategori = KategoriBarang::whereNull('deleted_at')->count();
+        $jumlahBarang = Barang::whereNull('deleted_at')->count();
+        $jumlahGudang = GudangDanToko::where('kategori_bangunan', 0)
+                                     ->whereNull('deleted_at')
+                                     ->count();
+        $jumlahSupplier = GudangDanToko::where('kategori_bangunan', 1)
+                                       ->whereNull('deleted_at')
+                                       ->count();
+        $jumlahToko = GudangDanToko::where('kategori_bangunan', 2)
+                                   ->whereNull('deleted_at')
+                                   ->count();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        // Mengirim data ke view
+        return view('dashboard.index', compact(
+            'jumlahKategori',
+            'jumlahBarang',
+            'jumlahGudang',
+            'jumlahSupplier',
+            'jumlahToko'
+        ));
     }
 }
